908c21b845890e4d6d4a3673330d550bb79710f0
Trevor Neri <trevor_neri@lineaje.com>
2023-08-21 15:38:58 -0600

    I think this fixed the image issue

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,19 +1,19 @@
 export default [
   {
     title: 'Portfolio Website',
-    imgUrl: 'src/assets/react.svg',
+    imgUrl: 'public/assets/react.svg',
     stack: ['React', 'Tailwind', 'TypeScript'],
     link: 'https://youtube.com',
   },
   {
     title: 'Portfolio Website',
-    imgUrl: 'src/assets/react.svg',
+    imgUrl: 'public/assets/react.svg',
     stack: ['React', 'Tailwind', 'TypeScript', 'Vite'],
     link: 'https://youtube.com',
   },
   {
     title: 'Portfolio Website',
-    imgUrl: 'src/assets/react.svg',
+    imgUrl: 'public/assets/react.svg',
     stack: ['React', 'Tailwind', 'TypeScript'],
     link: 'https://youtube.com',
   },
